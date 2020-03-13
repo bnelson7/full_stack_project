6cff81f668542cf54b4c62e79bb0f18212c6c621
your name here <bradnelson333@gmail.com>
2020-03-12 17:49:08 -0700

    node modules gone

--- frontend/components/videos/video_show.jsx
+++ frontend/components/videos/video_show.jsx
@@ -8,20 +8,43 @@ class VideoShow extends React.Component {
     }
 
     render() {
-        console.log(this.props)
+        // console.log(this.props)
         const { video } = this.props
         return(
             <div>
-                <h1>{video.title}</h1>
-            
-            {/* <div className="video-container">
-                <div className="video-desciption">
+                <div className="video-show-container">
 
+                    <div className="video-container">
+
+                    </div>
+
+                    <div className="video-title-container">
+
+                    </div>
+
+                    <div className="video-description-container">
+
+                    </div>
+
+                    <div className="upnext-video-container">
+
+                    </div>
+
+                    <div className="">
+
+                    </div>
+
+                    {/* <h1>{video.title}</h1> */}
+                
+                {/* <div className="video-container">
+                    <div className="video-desciption">
+
+                    </div>
                 </div>
-            </div>
-                <div className="recommended-videos">
+                    <div className="recommended-videos">
 
-                </div> */}
+                    </div> */}
+                </div>
             </div>
         )
     }
