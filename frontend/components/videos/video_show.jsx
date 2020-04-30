95c8636e4b801cd423595e4e4980cd638884efe5
your name here <bradnelson333@gmail.com>
2020-04-30 16:53:06 -0700

    video show styling

--- frontend/components/videos/video_show.jsx
+++ frontend/components/videos/video_show.jsx
@@ -2,22 +2,23 @@ import React from 'react'
 import { GoPrimitiveDot } from 'react-icons/go'
 import { FaUserCircle } from 'react-icons/fa'
 import VideoIndexItem from './video_index_item'
-
+import { IoMdThumbsUp, IoMdThumbsDown, IoMdShareAlt } from 'react-icons/io'
+import { MdPlaylistAdd, MdMoreHoriz, MdCheckCircle } from 'react-icons/md'
 
 class VideoShow extends React.Component {
     constructor(props) {
         debugger
         super(props)
 
+        this.state = {
+            collapsed: true
+        }
+
     }
 
     componentDidMount() {
         debugger
         this.props.requestVideo(this.props.match.params.videoId)
-        // .then(videos => {
-        //     debugger
-        //     this.setState({ videos: [videos] })
-        // })
     }
 
     componentDidUpdate(prevProps) {
@@ -43,15 +44,49 @@ class VideoShow extends React.Component {
                             </video>
                         </div>
                         <div className="video-title">
-                            <h1>{video.title}</h1>
-                            <span>{video.views}K views&nbsp;<span><GoPrimitiveDot /></span>&nbsp;{video.created_at}</span>
+                            <div className="video-title-info">
+                                <h1>{video.title}</h1>
+                                <span>{video.views}K views&nbsp;<span><GoPrimitiveDot /></span>&nbsp;{video.created_at}</span>
+                            </div>
+                            <div className="video-title-icons">
+                                <div>
+                                    <button>
+                                        <span id="video-title-icon"><IoMdThumbsUp /></span>
+                                        <span id="video-title-text">14K</span>
+                                    </button>
+                                    <button>
+                                        <span id="video-title-icon"><IoMdThumbsDown /></span>
+                                        <span id="video-title-text">1K</span>
+                                    </button>
+                                    <button>
+                                        <span id="video-title-icon"><IoMdShareAlt /></span>
+                                        <span id="video-title-text">SHARE</span>
+                                    </button>
+                                    <button>
+                                        <span id="video-title-icon"><MdPlaylistAdd /></span>
+                                        <span id="video-title-text">SAVE</span>
+                                    </button>
+                                </div>
+                                    <button className="horiz-dots">
+                                        <span id="video-title-icon"><MdMoreHoriz /></span>
+                                    </button>
+                            </div>
                         </div>
-                        <div className="video-description">
-                            <div className="profile-thumbnail">
+                        <div className="video-description-container">
+                            <div className="profile-thumbnail-show">
                                 <span><img src={video.creator.photoUrl} /></span>
                             </div>
-                            {/* {video.username} */}
-                            {video.description}
+                            <div className="video-description-text">
+                                <div className="video-description-title">
+                                    <h1>{video.creator.username}&nbsp;<span className="verified"><MdCheckCircle /></span></h1>
+                                    <p>no subscribers</p>
+                                </div>
+                                <div className="video-description">
+                                    {video.description}
+                                    <br/><span>SHOW MORE</span>
+                                </div>
+                            </div>
+                            <button className="subscribe-btn">SUBSCRIBE</button>
                         </div>
                     </div>
                     {/* <div className="related-container"> */}
@@ -59,7 +94,8 @@ class VideoShow extends React.Component {
 
                         </div> */}
                         <div className="suggested-videos">
-                            {videos.map(video => video.id !== videoId ? <li className="suggested-grid-item"><VideoIndexItem key={video.id} video={video} /></li> : null)}
+                            Up next
+                            {videos.filter(video => video.id !== videoId).map(video => <li className="suggested-grid-item"><VideoIndexItem key={video.id} video={video} /></li>)}
                         </div>
                     {/* </div> */}
                 </div>
