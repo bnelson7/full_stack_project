6a8f8cbafdbb34c2691b418e725aae631a2a02e1
your name here <bradnelson333@gmail.com>
2020-06-26 18:03:32 -0700

    add progress bar when page loads

--- app/assets/javascripts/progress_bar.js
+++ app/assets/javascripts/progress_bar.js
@@ -1,22 +1,22 @@
-
 document.onreadystatechange = function (e) {
-    if (document.readyState == "interactive") {
-        var all = document.getElementsByTagName("*");
-        for (var i = 0, max = all.length; i < max; i++) {
-            set_ele(all[i]);
+    debugger
+    if (document.readyState === "interactive") {
+        let all = document.getElementsByTagName("*");
+        debugger
+        for (let i = 0; i < all.length; i++) {
+            setEle(all[i]);
         }
     }
 }
 
-function check_element(ele) {
-    var all = document.getElementsByTagName("*");
-    var totalele = all.length;
-    var per_inc = 100 / all.length;
+function checkElement(ele) {
+    let all = document.getElementsByTagName("*");
+    let percentIncrease = 100 / all.length;
 
     if ($(ele).on()) {
-        var prog_width = per_inc + Number(document.getElementById("progress_width").value);
-        document.getElementById("progress_width").value = prog_width;
-        $("#bar1").animate({ width: prog_width + "%" }, 10, function () {
+        let progressWidth = percentIncrease + Number(document.getElementById("progress-width").value);
+        document.getElementById("progress-width").value = progressWidth;
+        $("#bar1").animate({ width: progressWidth + "%" }, 10, function () {
             if (document.getElementById("bar1").style.width == "100%") {
                 $(".progress").fadeOut("slow");
             }
@@ -24,12 +24,12 @@ function check_element(ele) {
     }
 
     else {
-        set_ele(ele);
+        setEle(ele);
     }
 }
 
-function set_ele(set_element) {
-    check_element(set_element);
+function setEle(setElement) {
+    checkElement(setElement);
 }
 
 
